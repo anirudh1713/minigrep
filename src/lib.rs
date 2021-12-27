c64b495dd5b051877d7c38c6d1ab88d20765ba4e
anirudh1713 <anirudhatodaria11@gmail.com>
2021-12-27 21:31:26 +0530

    fix linting issue

--- src/lib.rs
+++ src/lib.rs
@@ -2,23 +2,23 @@ use std::error::Error;
 use std::{fs, env};
 
 pub struct Config {
-  pub query: String,
-  pub filename: String,
-  pub case_sensitive: bool,
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
-  pub fn new(args: &[String]) -> Result<Config, &str> {
-    if args.len() < 3 {
-      return Err("not enough arguments.");
-    }
-    let query = args[1].clone();
-    let filename = args[2].clone();
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+        return Err("not enough arguments.");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
 
-    let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
-    
-    Ok(Config { query, filename, case_sensitive })
-  }
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        
+        Ok(Config { query, filename, case_sensitive })
+    }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
